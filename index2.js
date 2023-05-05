6bc0aa22fefa98f108c91de87309ce92c988f560
askmammad <119777238+askmammad@users.noreply.github.com>
2023-05-05 13:12:10 +0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,18 @@
+//first method
+
+let array = [4, 8, 9, 10, 1, 5, 3];
+function maximum(array) {
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+console.log(maximum(array));
+
+//second method
+
+let array1 = [4, 8, 9, 10, 1, 5, 3];
+console.log(Math.max(...array1));
